c021bddab563404262316163cff205d605db0b99
Raquel Ciusz <raquel.ciusz@yahoo.com>
2022-12-01 11:24:04 -0300

    calendar fixed both

--- app/javascript/controllers/open_calendar_controller.js
+++ app/javascript/controllers/open_calendar_controller.js
@@ -7,13 +7,14 @@ export default class extends Controller {
   static targets = ["modal", "modalBody"]
 
   connect() {
+    console.log('hey from open calendar')
     console.log(this.modalBodyTarget)
     this.modal = new bootstrap.Modal(this.modalTarget)
   }
 
   open({params}) {
     const url = `/requests/${params.id}/calendar`
-    
+
     fetch(url, {headers: {"Accept": "text/plain"}})
       .then(response => response.text())
       .then((data) => {
