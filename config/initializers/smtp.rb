705f2b0b6d78ad3390c2aa2113a1d5fbf17beccd
Raquel Ciusz <raquel.ciusz@yahoo.com>
2022-12-08 15:13:44 -0300

    passsword gmail update

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
   port: 587,
   domain: 'gmail.com',
   user_name: 'equipemyshift@gmail.com',
-  password: 'lewagonmyshift',
+  password: 'wktzbrggsmixccfe',
   authentication: :login,
   enable_starttls_auto: true
 }
