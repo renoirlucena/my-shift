02b94934d9c8fada2c722464930abfab40eb22fe
tais campos <tais.camposb@hotmail.com>
2022-12-08 10:10:23 -0300

    tentando

--- db/schema.rb
+++ db/schema.rb
@@ -51,7 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_07_150327) do
   end
 
   create_table "exchanges", force: :cascade do |t|
-    t.string "status", default: "Pendente"
+    t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
